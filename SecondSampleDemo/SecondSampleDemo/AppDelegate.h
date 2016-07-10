fbc03afdea28f136e368c3d4003213fd78b70d64
bear-zyy <13520592875@163.com>
2016-07-10 17:16:07 +0800

    第一次上传

---
+++ SecondSampleDemo/SecondSampleDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SecondSampleDemo
+//
+//  Created by 三个爸爸 on 16/7/10.
+//  Copyright © 2016年 张源远. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
